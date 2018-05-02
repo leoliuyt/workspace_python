8942b05738c91f027274d5d216b83c25c1ed2e35
lbq <lbq@LEO-MBP.local>
2018-05-02 19:05:45 +0800

    高级特性

---
+++ python_grammar/iteration.py
@@ -0,0 +1,13 @@
+dic = {'name':'leoliu','age':18}
+
+for key in dic:
+    print(key)
+
+for value in dic.values():
+    print(value)
+
+for key,value in dic.items():
+    print("key:",key,"value:",value)
+
+for x, y in [(1, 1), (2, 4), (3, 9)]:
+    print(x, y)
